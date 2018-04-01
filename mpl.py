6821f959ca6936a684ff1de41fca388a71c463f8
shaihuludata <sandworm1986@gmail.com>
2018-04-02 00:53:50 +0700

    сделал наблюдателя, нужно в нём реализовать карту сигналов и вывод результатов очередирование ONT

--- mpl.py
+++ mpl.py
@@ -1,14 +1,24 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mp
 #mp.use('agg')
 
+#plt.dr
+plt.ion()
 fig = plt.figure(1, figsize=(9, 6))
 ax = fig.add_subplot(111)
-data_to_plot = [np.array([1,2,3]), np.array([4,6]), np.array([5, 10])]
-bp = ax.boxplot(data_to_plot, capprops=dict(color="red"))
-
+#data_to_plot = [np.array([1, 3]), np.array([4, 6]), np.array([5, 10])]
+data_to_plot = [[1, 3], [4, 6], [5, 10]]
 fig.show()
+#plt.show(fig)
+for dtp in data_to_plot:
+    #bp = ax.boxplot(dtp)#, capprops=dict(color="red"))
+    ax.boxplot(dtp)
+    fig.canvas.draw()
+    print(plt.isinteractive())
+    time.sleep(1)
+
 #fig.savefig('fig1.png', bbox_inches='tight')
-import time
-time.sleep(10)
+#import time
+#time.sleep(10)
--- observers.py
+++ observers.py
@@ -13,8 +13,6 @@ class Observer:
         for time in passed_schedule:
             for event in passed_schedule[time]:
                 dev, state, sig, port = event['dev'], event['state'], event['sig'], str(event['port'])
-                if 'OLT' in dev.name:
-                    print('ку')
                 point = dev.name + '::' + port
                 if point in self.observer_result:
                     time_sig = self.observer_result[point]
@@ -23,3 +21,19 @@ class Observer:
                 time_sig.append((time, state, sig.__dict__))
                 self.observer_result[point] = time_sig
         return
+
+    def make_results(self):
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import matplotlib as mp
+        #mp.use('agg')
+
+        fig = plt.figure(1, figsize=(9, 6))
+        ax = fig.add_subplot(111)
+        data_to_plot = [np.array([1, 2, 3]), np.array([4, 6]), np.array([5, 10])]
+        bp = ax.boxplot(data_to_plot)#, capprops=dict(color="red"))
+
+        fig.show()
+        #fig.savefig('fig1.png', bbox_inches='tight')
+        import time
+        time.sleep(10)
