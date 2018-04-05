fb173fc18a0a16b286af067043a0f4f5b6faae72
shaihuludata <sandworm1986@gmail.com>
2018-04-05 16:02:30 +0700

    Очередирование на ONT диаграмма состояний ONT и включение в активность в процессе

--- scheduler.py
+++ scheduler.py
@@ -3,83 +3,41 @@ from active_optics import Olt, Ont
 from observers import Observer
 #schedule = {time : [event]}
 
-# class Schedule:
-#     def __init__(self):
-#         self.a = 'a'
-#
-#     def upd_schedule(self, ev: dict):
-#         start_schedule = dict()
-#         start_schedule.update(schedule)
-#         new_time_schedule = dict()
-#         if None in schedule.values():
-#             print('вот оно')
-#         for time in ev:
-#             if time not in schedule:
-#                 new_time_schedule.update({time: ev[time]})
-#             elif schedule[time] is None:
-#                 new_time_schedule.update({time: ev[time]})
-#             else:
-#                 old_events = schedule[time]
-#                 new_events = ev[time]
-#                 for event in new_events:
-#                     if event not in old_events:
-#                         # result_events = old_events.append(event)
-#                         old_events.append(event)
-#                         schedule[time] = old_events
-#                     else:
-#                         print('Планируемое событие уже есть в базе')
-#         schedule.update(new_time_schedule)
-#         if None in schedule.values():
-#             print('вот оно')
-#         return schedule
-#
-#     def del_event_from_schedule(schedule, new_events):
-#         # if ev not in schedule:
-#         #     schedule.update(new_events)
-#         # elif ev in self.schedule and self.schedule[ev] != new_events[ev]:
-#         #     self.schedule[ev].extend(new_events[ev])
-#         # else:
-#         #     print('Планируемое событие уже есть в базе')
-#         # new_schedule = dict()
-#         # return new_schedule
-#         pass
+class Schedule:
+    events = dict()
 
-def upd_schedule(schedule: dict, ev: dict):
-    start_schedule = dict()
-    start_schedule.update(schedule)
-    new_time_schedule = dict()
-    if None in schedule.values():
-        print('вот оно')
-    for time in ev:
-        if time not in schedule:
-            new_time_schedule.update({time: ev[time]})
-        elif schedule[time] is None:
-            new_time_schedule.update({time: ev[time]})
-        else:
-            old_events = schedule[time]
-            new_events = ev[time]
-            for event in new_events:
-                if event not in old_events:
-                    #result_events = old_events.append(event)
-                    old_events.append(event)
-                    schedule[time] = old_events
-                else:
-                    print('Планируемое событие уже есть в базе')
-    schedule.update(new_time_schedule)
-    if None in schedule.values():
-        print('вот оно')
-    return schedule
+    def upd_schedule(self, new_events:dict):
+        start_schedule = dict()
+        start_schedule.update(self.events)
+        new_time_schedule = dict()
+        for time in new_events:
+            if time not in self.events:
+                new_time_schedule.update({time: new_events[time]})
+            elif self.events[time] is None:
+                new_time_schedule.update({time: new_events[time]})
+            else:
+                old_events = self.events[time]
+                events = new_events[time]
+                for event in events:
+                    if event not in old_events:
+                        old_events.append(event)
+                        self.events[time] = old_events
+                    # else:
+                    #     print('Планируемое событие уже есть в базе')
+        self.events.update(new_time_schedule)
+        if None in self.events.values():
+            print('Ошибка в расписании!')
 
-def del_event_from_schedule(schedule, new_events):
-    # if ev not in schedule:
-    #     schedule.update(new_events)
-    # elif ev in self.schedule and self.schedule[ev] != new_events[ev]:
-    #     self.schedule[ev].extend(new_events[ev])
-    # else:
-    #     print('Планируемое событие уже есть в базе')
-    # new_schedule = dict()
-    # return new_schedule
-    pass
+    def del_event_from_schedule(schedule, new_events):
+        # if ev not in schedule:
+        #     schedule.update(new_events)
+        # elif ev in self.schedule and self.schedule[ev] != new_events[ev]:
+        #     self.schedule[ev].extend(new_events[ev])
+        # else:
+        #     print('Планируемое событие уже есть в базе')
+        # new_schedule = dict()
+        # return new_schedule
+        pass
 
 
 class ModelScheduler:
@@ -108,13 +66,13 @@ class ModelScheduler:
         # self.dba_algorithm = algorithm
         self.current_requests = list()
         self.time = -125
-        self.schedule = dict()
-        self.schedule = upd_schedule(self.schedule, self.interrogate_devices())
+        self.schedule = Schedule()
+        self.schedule.upd_schedule(self.interrogate_devices())
         return
 
     def notify_observers(self):
         for observer in self.observers:
-            observer.notice(self.schedule, self.time)
+            observer.notice(self.schedule.events, self.time)
 
     def make_results(self):
         for observer in self.observers:
@@ -123,8 +81,8 @@ class ModelScheduler:
     def proceed_schedule(self, cur_time):
         self.time = cur_time
         self.notify_observers()
-        current_events = self.schedule.pop(cur_time)
-        new_events = dict()
+        current_events = self.schedule.events.pop(cur_time)
+        new_sched = Schedule()
         for event in current_events:
             state, sig = event['state'], event['sig']
             l_dev, l_port = event['dev'], event['port']
@@ -136,7 +94,7 @@ class ModelScheduler:
                 r_device, r_port = self.net[l_device]['ports'][str(l_port)].split("::")
                 r_dev = self.devices[r_device]
                 new_event = {'dev': r_dev, 'state': 'r_start', 'sig': sig, 'port': int(r_port)}
-                new_events = upd_schedule(new_events, {cur_time: [new_event]})
+                new_sched.upd_schedule({cur_time: [new_event]})
             elif state == 'r_start':
                 port_sig_dict = l_dev.r_start(sig, l_port)
                 if ('ONT' in l_dev.name or 'OLT' in l_dev.name) and len(port_sig_dict) == 1:
@@ -149,7 +107,7 @@ class ModelScheduler:
                         new_event = {'dev': l_dev, 'state': 's_start',
                                      'sig': port_sig_dict[port]['sig'], 'port': port}
                         delay = port_sig_dict[port]['delay']
-                        new_events = upd_schedule(new_events, {cur_time + delay: [new_event]})
+                        new_sched.upd_schedule({cur_time + delay: [new_event]})
             elif state == 's_end':
                 l_device, l_port, sig = l_dev.s_end(sig, l_port)
                 try:
@@ -158,7 +116,7 @@ class ModelScheduler:
                     raise
                 r_dev = self.devices[r_device]
                 new_event = {'dev': r_dev, 'state': 'r_end', 'sig': sig, 'port': int(r_port)}
-                new_events = upd_schedule(new_events, {cur_time: [new_event]})
+                new_sched.upd_schedule({cur_time: [new_event]})
             elif state == 'r_end':
                 port_sig_dict = l_dev.r_end(sig, l_port)
                 if ('ONT' in l_dev.name or 'OLT' in l_dev.name) and len(port_sig_dict) == 1:
@@ -173,20 +131,20 @@ class ModelScheduler:
                         new_event = {'dev': l_dev, 'state': 's_end',
                                      'sig': port_sig_dict[port]['sig'], 'port': port}
                         delay = port_sig_dict[port]['delay']
-                        new_events = upd_schedule(new_events, {cur_time + delay: [new_event]})
+                        new_sched.upd_schedule({cur_time + delay: [new_event]})
             else:
                 raise Exception('{} Non implemented'.format(state))
-        self.schedule = upd_schedule(self.schedule, new_events)
-        self.schedule = upd_schedule(self.schedule, self.interrogate_devices())
+        self.schedule.upd_schedule(new_sched.events)
+        self.schedule.upd_schedule(self.interrogate_devices())
         return True
 
     def interrogate_devices(self):
-        new_events = dict()
+        new_sched = Schedule()
         for dev_name in self.devices:
             dev = self.devices[dev_name]
             event = None
             if 'OLT' in dev.name or 'ONT' in dev.name:
                 event = dev.plan_next_act(self.time)
             if event is not None:
-                new_events = upd_schedule(new_events, event)
-        return new_events
+                new_sched.upd_schedule(event)
+        return new_sched.events
