a6db2fc720a9bcb45164d0bce0d0b45608a980d5
shaihuludata <sandworm1986@gmail.com>
2018-04-08 16:31:35 +0700

    1. очередирование ONT надо делать 2. механизм авторизации ont на olt в процессе

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     net = json.load(open('./networks/network4.json'))
     print('Net description: ', net)
     sched = ModelScheduler(net)
-    time_horisont = 1500
+    time_horisont = 15000
     cur_time = 0
     while cur_time < time_horisont and len(sched.schedule.events) > 0:
         cur_time = min(sched.schedule.events)
@@ -15,6 +15,8 @@ def main():
         #print(sched.schedule)
         sched.proceed_schedule(cur_time)
         #sleep(1)
+        if cur_time > 10000:
+            pass
 
     sched.make_results()
     print('End of simulation')
