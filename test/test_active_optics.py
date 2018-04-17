1a460cabbd43c4496a02196c8fad1ca4fcbd2937
shaihuludata <sandworm1986@gmail.com>
2018-04-17 18:50:06 +0700

    Очередирование на ONT в процессе traffic_monitoring dba on the go

--- test/test_active_optics.py
+++ test/test_active_optics.py
@@ -1,25 +1,25 @@
-# from pon_device import PonDevice
-# from collections import OrderedDict
-from signal import Signal
-# import random
-from active_ont import Ont
-
-class test_Ont(Ont):
-
-    def plan_next_act(self, time):
-        #эту часть надо поместить в тестирование
-        if "time_start" in self.config and "time_end" in self.config:
-            time_start = int(self.config["time_start"])
-            time_end = int(self.config["time_end"])
-            data = 'bugaga'
-            sig = Signal('{}:{}:{}'.format(time, self.name, time_start), data)
-            if time_start in self.sending_sig:
-                return {}
-            else:
-                self.sending_sig[time_start] = sig.id
-                return {time_start: [{"dev": self, "state": "s_start", "sig": sig, "port": 0}],
-                        time_end: [{"dev": self, "state": "s_end", "sig": sig, "port": 0}]
-                        }
-        else:
-            print('Тестовое время старта не установлено')
-
+# # from pon_device import PonDevice
+# # from collections import OrderedDict
+# from signal import Signal
+# # import random
+# from active_ont import Ont
+#
+# class test_Ont(Ont):
+#
+#     def plan_next_act(self, time):
+#         #эту часть надо поместить в тестирование
+#         if "time_start" in self.config and "time_end" in self.config:
+#             time_start = int(self.config["time_start"])
+#             time_end = int(self.config["time_end"])
+#             data = 'bugaga'
+#             sig = Signal('{}:{}:{}'.format(time, self.name, time_start), data)
+#             if time_start in self.sending_sig:
+#                 return {}
+#             else:
+#                 self.sending_sig[time_start] = sig.id
+#                 return {time_start: [{"dev": self, "state": "s_start", "sig": sig, "port": 0}],
+#                         time_end: [{"dev": self, "state": "s_end", "sig": sig, "port": 0}]
+#                         }
+#         else:
+#             print('Тестовое время старта не установлено')
+#
