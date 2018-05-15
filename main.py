8c051055314c0f7859cbc8fafa791ae651ae154d
shaihuludata <sandworm1986@gmail.com>
2018-05-15 14:53:33 +0800

    нужен какой-то непредсказуемый тип трафика...

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def main():  # *args, **kwargs):
     # for t in sched.olt.dba.global_bwmap:
     #     map = sched.olt.dba.global_bwmap[t]
     #     print('{} {}'.format(t, map))
-
     print('End of simulation... Preparing results.')
     sched.make_results()
 
