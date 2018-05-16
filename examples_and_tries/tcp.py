ef6588feb5921bd5d8eab467c18f330eef373333
shaihuludata <sandworm1986@gmail.com>
2018-05-17 01:32:11 +0800

    обдумываю рефакторинг на simpy

---
+++ examples_and_tries/tcp.py
@@ -0,0 +1,40 @@
+
+#!/usr/bin/env python
+
+# Client
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+print("received data:", data)
+
+
+#!/usr/bin/env python
+
+# Server
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connection address:', addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print("received data:", data)
+    conn.send(data)  # echo
+conn.close()
--- examples_and_tries/timing_objects.py
+++ examples_and_tries/timing_objects.py
@@ -1,23 +1,38 @@
 
+class Clock:
+    def __init__(self, start):
+        self.t = start
+
 
 class Sched:
-    def __init__(self, time: float):
-        self.time = time
+    def __init__(self, t: float, objects):
+        clock = Clock(t)
+        self.t = clock.t
         self.devs = list()
         self.attr = dict()
         self.attr['time'] = 0
 
-    def init_devices(self, objects):
         for o_name in objects:
-            o = a(o_name)
-            o.time = self.time
-            o.attr = self.attr
+            o = A(o_name)
+            o.t = clock.t
             self.devs.append(o)
 
 
-class a:
+class A:
     def __init__(self, o_name):
-        self.time = float()
+        self.t = float()
         self.name = o_name
         self.attr = dict()
 
+# import examples_and_tries.timing_objects as to
+# sc = to.Sched(0, ["a", "b", "c"])
+
+
+# def main():
+#     sc = Sched(0, ["a", "b", "c"])
+#     print(sc.t)
+#     print()
+#
+#
+# if __name__ == '__main__':
+#     main()
